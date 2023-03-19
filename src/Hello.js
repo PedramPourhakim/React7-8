063103d957728e12e227fedb2d2a08a50500f0f5
Pedram Pourhakim <shemuel1226@gmail.com>
2023-03-19 16:29:36 +0330

    until session11

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './style.css'
+
+class Hello extends React.Component{
+    render(){
+      return(
+        <h1>پدرام پورحکیم</h1>
+      )
+    }
+  }
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style.css'
 
-class Timer extends React.Component{
-  render(){
-    return(
-      <h2>
-          it is {new Date().toLocaleTimeString()}
-      </h2>
-    )
-  }
-}
-class Hello extends React.Component{
-  render(){
-    return(
-      <h1>سلام</h1>
-    )
-  }
-}
+import App from './App';
 
-class App extends React.Component {
-  render(){
-    return(
-      <div>
-        <Hello/>
-        <Timer/>
-      </div>
-    )
-  }
-}
-
-// let elem = new App()
-const tick =()=>{
   const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // elem.render()
   <App/>
 );
-}
-setInterval(()=>{
-  tick();
-},1000)
+
 
 
